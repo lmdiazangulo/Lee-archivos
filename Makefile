911701250ba8244012d9d15ec0480219b5b74a21
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2018-01-31 12:57:59 +0100

    Code is simplified.

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: lee_archivos
+	g++ -O0 -g3 -o lee_archivos lee_archivos.cpp 
--- lee_archivos.cpp
+++ lee_archivos.cpp
@@ -4,43 +4,21 @@ using namespace std;
 
 int main(void) {
 
-	//declaro variables
+	ifstream archivo1("datos1.dat");
+	ifstream archivo2("datos2.dat");
+	ofstream archivo3("productos.dat");
 
-	int data1[4],data2[4],product[4];
-	int i;
-
-
-	data1[0] = 9;
-	//ifstream es para archivos de lectura
-	//ofstream es para archivos de escritura
-
-	ifstream archivo1,archivo2;
-	ofstream archivo3;
-
-	//abro los ficheros indicando si son de lectura o escritura
-
-
-	archivo1.open("datos1.dat",ios::in);
-	archivo2.open("datos2.dat",ios::in);
-	archivo3.open("productos.dat",ios::out);
-
-	//leo los datos de los ficheros de lectura y escribo en archivo3
-
-	for(i=0;i<4;i++){
-		archivo1 >> data1[i];
-		archivo2 >> data2[i];
-		product[i] = data1[i]*data2[i];
-		archivo3 << product[i] << endl;
+	for(int i = 0; i < 4; ++i) {
+	    int data1, data2;
+		archivo1 >> data1;
+		archivo2 >> data2;
+		archivo3 << (data1 * data2) << endl;
 	}
 
-	//cierro archivos
-
-	archivo1.close();
-	archivo2.close();
 	archivo3.close();
 
-
-	}
+	return 0;
+}
 
 
 
